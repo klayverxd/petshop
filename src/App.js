1eaec19d8599b12aed469e6fe6240aa0a9952425
Klayver Ximenes <klayverx@gmail.com>
2021-05-07 14:50:43 -0300

    listagem dos posts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 import './App.css';
+import './assets/css/base/base.css';
+
+import Home from './pages/Home';
+import Sobre from './pages/Sobre';
+
+import Pagina404 from './pages/Pagina404';
+import Cabecalho from './components/Cabecalho';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Cabecalho />
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route path="/sobre">
+					<Sobre />
+				</Route>
+				<Route>
+					<Pagina404 />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
